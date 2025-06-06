c32df9b9daeab3f417d70c4716bebe81d3dea4ec
dipsonpaul <dipsonpaul202@gmail.com>
2025-06-06 10:40:50 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -18,7 +18,7 @@ import TaskManagement from "./pages/TaskManagement";
 import SalesManagement from "./pages/SalesManagement";
 import ResourceManagement from "./pages/ResourceManagement";
 import Estimation from "./pages/Estimation";
-import EmployeeDetail from "./pages/EmployeeDetail";
+import EmployeeDetails from "./pages/EmployeeDetails";
 
 const queryClient = new QueryClient();
 
@@ -42,7 +42,7 @@ const App = () => (
           <Route path="/sales" element={<SalesManagement />} />
           <Route path="/resource-management" element={<ResourceManagement />} />
           <Route path="/estimation" element={<Estimation />} />
-          <Route path="/employee/:id" element={<EmployeeDetail />} />
+          <Route path="/employee/:id" element={<EmployeeDetails />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
         </Routes>
